e8b482dd5a292fe8efe37c2256d970944b7c529e
Sahilminhas07 <98466660+Sahilminhas07@users.noreply.github.com>
2023-07-01 14:40:57 +0530

    Add files via upload

---
+++ class2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class student{
+	string passcode;
+	public:
+		string name;
+		int id;
+		student(){
+		}
+		student(string passcode, string name,int id){
+			this->name = name;
+			this->id = id;
+			this->passcode= passcode;
+		}
+
+		void into(){
+			cout<<"My name is "<<name<<" , my id is "<<id<<" passcode is "<<passcode;
+
+		}
+		void setPass(string s){
+			passcode= s;
+		}
+};
+
+
+int main(){
+	student s1;
+	student s2("10","Mohit",1);
+	student s3;
+	s3 = s2;
+	s3.into();
+	return 0;
+}
